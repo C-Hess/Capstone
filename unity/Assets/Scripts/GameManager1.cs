76f59c8214e19bcf9e760055ca5dfe4ccae002a3
link46223 <blstackboy@gmail.com>
2019-10-03 14:49:16 -0500

    Moving over to my desktop
    Start State
    End State
    Game State

---
+++ unity/Assets/Scripts/GameManager1.cs
@@ -0,0 +1,37 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.SceneManagement;
+
+public class GameManager1 : MonoBehaviour
+{
+
+    public float restart = 1f;
+    public float newlevel = 2f;
+    public double levelDiff = 1.0;
+    public int levelNumber = 1;
+    public int score = 0;
+
+    public void GameOver()
+    {
+        Debug.Log("Game Over");
+    }
+
+    public void LevelWon()
+    {
+        Debug.Log("Level Complete!");
+        levelNumber++;
+        Invoke("Restart", newlevel);
+    }
+
+    public void RestartGame()
+    {
+        Debug.Log("Restarting game...");
+        Invoke("Restart", restart);
+    }
+
+    void Restart()
+    {
+        SceneManager.LoadScene(SceneManager.GetActiveScene().buildIndex);
+    }
+}
