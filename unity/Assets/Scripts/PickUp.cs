587d7b21c99a3e9edc6c485ca8ef5eb113a6c702
Kyle <kyle.j.smith1996@gmail.com>
2019-10-04 03:57:34 -0500

    Pickup the wirecutters
    This does not work yet. Possibly have a script so that you can pick up and set down the wires? Who knows

---
+++ unity/Assets/Scripts/PickUp.cs
@@ -0,0 +1,24 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class PickUp : MonoBehaviour
+{
+    // Start is called before the first frame update
+    public Transform theDestination;
+
+    void OnMouseDown()
+    {
+        GetComponent<Rigidbody>().useGravity = false;
+        this.transform.position = theDestination.position;
+        this.transform.parent = GameObject.Find("Destination").transform;
+
+    }
+
+    void OnMouseUp()
+    {
+        this.transform.parent = null;
+        GetComponent<Rigidbody>().useGravity = true;
+
+    }
+}
